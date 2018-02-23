d5e58fd2a183fe8160f44e7bb62407048e7ee013
Corentin Noël <corentin@elementary.io>
2018-02-24 00:13:36 +0100

    meson: add Meson build system

--- config.vala.cmake
+++ config.vala.cmake
@@ -1,7 +1,3 @@
 namespace Constants {
-public const string PLUGNAME = "@PLUGNAME@";
-public const string DATADIR = "@DATADIR@";
 public const string PLANKDATADIR = "@PLANKDATADIR@";
-public const string PKGDATADIR = "@PKGDATADIR@";
-public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
 }
